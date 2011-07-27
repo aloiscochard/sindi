9170f597e48790425b04a031d6dcc6551bcd56b5
Alois Cochard <alois.cochard@gmail.com>
2011-07-27 20:47:38 +0200

    Added nerdy ASCII art header

--- src/main/scala/sindi/injector.scala
+++ src/main/scala/sindi/injector.scala
@@ -1,3 +1,13 @@
+//      _____         ___  
+//     / __(_)__  ___/ (_)
+//    _\ \/ / _ \/ _  / /
+//   /___/_/_//_/\_,_/_/
+//
+//  (c) 2011, Alois Cochard
+//
+//  http://aloiscochard.github.com/sindi
+//
+
 package sindi
 package injector
 
