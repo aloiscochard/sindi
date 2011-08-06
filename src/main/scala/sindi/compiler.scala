82ea30dda4827e775a694522212a8580e82335ea
aloiscochard <alois.cochard@gmail.com>
2011-08-06 11:46:42 +0200

    Quest for The Almighty Scala AST - Part 2

--- src/main/scala/sindi/compiler.scala
+++ src/main/scala/sindi/compiler.scala
@@ -39,26 +39,7 @@ class DependencyChecker(val global: Global) extends Plugin {
 
       def apply(unit: CompilationUnit) {
 
-        // TODO [aloiscochard] @tailrec
-        def implement[T : Manifest](tree: Tree): Boolean = {
-          if (tree.tpe.toString == manifest[T].erasure.getName) return true
-          tree match {
-            case tree: ClassDef => {
-              tree.impl.parents.foreach((p) => {
-                if (implement[T](p)) return true
-              })
-              return false
-            }
-            case _ => false
-          }
-        }
-
-        @tailrec def test(a: Int): Int = {
-          if (a == 1) return 0
-          return test(a + 1)
-        }
-
-        @tailrec def lookup(matcher: (Tree) => Boolean, trees: Tree*): Option[Tree] = {
+        @tailrec def find(matcher: (Tree) => Boolean, trees: Tree*): Option[Tree] = {
           var children = List[Tree]()
           trees.flatMap((tree: Tree) => {
             if (matcher(tree)) {
@@ -72,52 +53,124 @@ class DependencyChecker(val global: Global) extends Plugin {
             case _ =>
           }
           if (children.isEmpty) return None
-          return lookup(matcher, children:_*)
+          return find(matcher, children:_*)
         }
 
-        /*
-        def lookup(tree: Tree, matcher: (Tree) => Boolean): Option[Tree] = {
-          if (matcher(tree)) {
-            return Some(tree)
-          } else {
-            tree.children.foreach((tree: Tree) =>
-              lookup(tree, matcher) match {
-                case Some(tree) => return Some(tree)
-                case _ =>
-              }
-            )
-            return None
-          }
+        def implement[T : Manifest](tree: Tree): Boolean = {
+          find((tree) => {
+            tree.tpe.toString == manifest[T].erasure.getName
+          }, tree).isDefined
         }
-        */
+
 
         def isComponent(tree: Tree) = implement[sindi.Component](tree)
         def isModule(tree: Tree) = implement[sindi.Module](tree)
+
+        def getBindings(tree: ClassDef): List[Type] = {
+          var bindings = List[Type]()
+          for (tree @ ValDef(_, _, _, _) <- tree.impl.body) {
+            if (tree.tpt.tpe.toString == "List[sindi.binder.binding.Binding[_]]" ||
+                tree.tpt.tpe.toString == "sindi.package.Bindings") {
+              //println(tree.rhs)
+              for (tree @ Apply(_, _) <- tree.children) {
+                // Collect all TypeTree, typeTree.tpe = Bound
+                @tailrec def collect(lookup: List[Tree], accumulator: List[TypeTree] = Nil): List[TypeTree] = {
+                  var children = List[Tree]()
+                  val found = for(tree <- lookup) yield {
+                    children = children ++ tree.children
+                    tree match {
+                      case tree: TypeApply => {
+                        if (tree.symbol.owner.toString == "trait DSL" && tree.symbol.name.toString == "bind") {
+                          var f: Option[TypeTree] = None
+                          for (t <- tree.children) {
+                            t match {
+                              case t: TypeTree => f = Some(t)
+                              case _ => 
+                            }
+                          }
+                          f
+                        } else {
+                          None
+                        }
+                      }
+                      case _ => None
+                    }
+                  }
+                  if (children.isEmpty) {
+                    accumulator
+                  } else {
+                    collect(children, found.flatten ++ accumulator)
+                  }
+                }
+                collect(tree.children).foreach((tree) => {
+                  bindings = tree.tpe :: bindings
+                })
+              }
+            }
+          }
+          bindings
+        }
+
+        def getDependencies(tree: Tree): List[Tuple2[Type, Type]] = {
+            for (tree @ DefDef(_, _, _, _, _, _) <- tree.children.head.children) yield {
+              var injected: Type = null
+              var module: Type = null
+              find((tree) => {
+                tree match {
+                  case apply: Apply => 
+                      apply.symbol.owner.toString == "trait Injector" && apply.symbol.name.toString == "inject"
+                  case _ => false
+                }
+              }, tree) match {
+                case Some(tree) => {
+                  injected = tree.tpe
+                  find((tree) => {
+                    tree match {
+                      case typeApply: TypeApply =>
+                        typeApply.symbol.name.toString == "from"
+                      case _ => false
+                    }
+                  }, tree) match {
+                    case Some(tree) =>
+                      find((tree) => {
+                        tree match {
+                          case typeTree: TypeTree => true
+                          case _ => false
+                        }
+                      }, tree) match {
+                        case Some(tree) => {
+                          module = tree.tpe
+                        }
+                        case _ =>
+                      }
+                    case _ =>
+                  }
+                }
+                case None =>
+              }
+              (injected, module)
+            } 
+        }
 
         for (tree @ ClassDef(_, _, _, _) <- unit.body) {
           //println(tree.name)
           //println(tree.impl.parents)
           if (isModule(tree)) {
             println("[module]" + tree.name)
-            for (tree @ ValDef(_, _, _,  _) <- tree.impl.body) {
-              if (tree.tpt.tpe.toString == "List[sindi.binder.binding.Binding[_]]" ||
-                  tree.tpt.tpe.toString == "sindi.package.Bindings") {
-                //println(tree.rhs)
-              }
-            }
+            getBindings(tree).foreach((t) => {
+              println("<- %s".format(t))
+            })
           }
           if (isComponent(tree)) {
             println("[component]" + tree.name)
-
-
-            for (tree @ DefDef(_, _, _, _, _, _) <- tree.impl.body) {
-              println(tree.rhs)
-              //DependencyChecker.this.global.treeBrowsers.create().browse(tree) 
-            }
+            getDependencies(tree).foreach((d) => {
+              val (injected, module) = d
+              println("%s -> %s".format(module, injected))
+            })
           }
         }
-        // allows to browse the whole AST
-        //DependencyChecker.this.global.treeBrowsers.create().browse(unit.body) 
+
+        //DependencyChecker.this.global.treeBrowsers.create().browse(tree) 
       }
     }
   }
