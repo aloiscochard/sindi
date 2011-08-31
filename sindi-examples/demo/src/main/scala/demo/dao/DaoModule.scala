d18d49dbc8c2a613c01158494aae710287ac7bc1
aloiscochard <alois.cochard@gmail.com>
2011-08-31 13:45:22 +0200

    Corrected demo

--- sindi-examples/demo/src/main/scala/demo/dao/DaoModule.scala
+++ sindi-examples/demo/src/main/scala/demo/dao/DaoModule.scala
@@ -8,10 +8,10 @@ import store._
 class DaoModule(implicit val context: Context) extends Module { 
   override lazy val modules = new StoreModule[Task](this) :: new StoreModule[User](this) :: Nil
    
-  def users = from[StoreModule[User]].inject[Store[User]]
-  def tasks = from[StoreModule[Task]].inject[Store[Task]]
+  lazy val users = from[StoreModule[User]].inject[Store[User]]
+  lazy val tasks = from[StoreModule[Task]].inject[Store[Task]]
 }
 
-trait UserComponent extends Component[DaoModule] { lazy val users = from[DaoModule].users }
+trait UserComponent extends Component[DaoModule] { def users = from[DaoModule].users }
 
-trait TaskComponent extends Component[DaoModule] { def tasks = from[DaoModule].inject[Store[Task]] }
+trait TaskComponent extends Component[DaoModule] { def tasks = from[DaoModule].tasks }
