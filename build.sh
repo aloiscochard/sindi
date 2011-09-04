ed17276b39f72c828ee1a2e80654c9b63d547e75
Alois Cochard <alois.cochard@gmail.com>
2011-09-04 19:01:39 +0200

    Script Fixes

--- build.sh
+++ build.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 sbt 'project sindi-compiler' +clean-files +clean +proguard +publish-local
 rename -f s/.min.jar/.jar/ sindi-compiler/target/scala*/sindi-compiler*.min.jar
-sbt +publish-local
+sbt 'project sindi-compiler' +publish-local
 sbt 'project sindi-core' +clean-files +clean +publish-local
--- publish.sh
+++ publish.sh
@@ -1,9 +1,9 @@
 #!/bin/sh
 sbt test
 echo
-read -p "Press any key to start publishing..."
+read -p "Press <ENTER> to start publishing..." x
 echo
-sbt 'project sindi-compiler' +clean-files +clean +proguard +publish
+sbt 'project sindi-compiler' +clean-files +clean +proguard +publish-local
 rename -f s/.min.jar/.jar/ sindi-compiler/target/scala*/sindi-compiler*.min.jar
-sbt +publish
+sbt 'project sindi-compiler' +publish
 sbt 'project sindi-core' +clean-files +clean +publish
