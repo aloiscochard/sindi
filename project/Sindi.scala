54df3d792e2627309d2e07bd91ea1db6da99ad01
Alois Cochard <alois.cochard@gmail.com>
2012-06-08 21:38:16 +0100

    Upgraded Scala support up to version 2.9.2 and SBT support to 0.11.3

--- project/Sindi.scala
+++ project/Sindi.scala
@@ -7,9 +7,9 @@ object BuildSettings {
   val buildSettings = Defaults.defaultSettings ++ Sonatype.settings ++ Seq(
     organization        := "com.github.aloiscochard.sindi",
     version             := "0.6-SNAPSHOT",
-    scalaVersion        := "2.9.1",
+    scalaVersion        := "2.9.2",
     scalacOptions       := Seq("-unchecked", "-deprecation"),
-    crossScalaVersions  := Seq("2.9.0-1", "2.9.1")
+    crossScalaVersions  := Seq("2.9.0", "2.9.0-1", "2.9.1", "2.9.1-1", "2.9.2")
   )
 }
 
@@ -23,8 +23,11 @@ object Resolvers {
 object Dependencies {
   val testDependencies = Seq(libraryDependencies <<= (scalaVersion, libraryDependencies) { (version, dependencies) =>
     val specs2 = version match {
-      case "2.9.1" => ("org.specs2" %% "specs2" % "1.6.1" % "test")
-      case _ => ("org.specs2" %% "specs2" % "1.5" % "test")
+      case "2.9.0" => ("org.specs2" %% "specs2" % "1.7.1" % "test")
+      case "2.9.0-1" => ("org.specs2" %% "specs2" % "1.8.2" % "test")
+      case "2.9.1" => ("org.specs2" %% "specs2" % "1.9" % "test")
+      case "2.9.1-1" => ("org.specs2" %% "specs2" % "1.9" % "test")
+      case _ => ("org.specs2" %% "specs2" % "1.11" % "test")
     }
     dependencies :+ specs2
   })
--- project/plugins.sbt
+++ project/plugins.sbt
@@ -1,6 +1,6 @@
 resolvers += "Maven Central" at "http://repo1.maven.org/maven2"
 
-libraryDependencies <+= sbtVersion(v => "com.github.siasia" %% "xsbt-proguard-plugin" % (v+"-0.1.1"))
+libraryDependencies <+= sbtVersion(v => "com.github.aloiscochard" %% "xsbt-proguard-plugin" % (v+"-0.1.2"))
 
 addSbtPlugin("com.github.aloiscochard" %% "xsbt-fmpp-plugin" % "0.2")
 
